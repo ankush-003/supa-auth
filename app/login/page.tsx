248eb7922d4f62e75181ae917e02a61a5e152ee0
ankush-003 <94037471+ankush-003@users.noreply.github.com>
2023-11-06 17:07:25 +0530

    added oauth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -52,6 +52,28 @@ export default function Login({
     return redirect('/login?message=Check email to continue sign in process')
   }
 
+  const googleSignIn = async () => {
+    'use server'
+    const origin = headers().get('origin')
+    const cookieStore = cookies()
+    const supabase = createClient(cookieStore)
+
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: `${origin}/auth/callback`
+      },
+    })
+
+    // console.log(data, error)
+
+    if (error) {
+      return redirect('/login?message=Could not authenticate user')
+    }
+
+    return redirect(data.url)
+  }
+
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
       <Link
@@ -107,11 +129,20 @@ export default function Login({
         >
           Sign Up
         </button>
+        
         {searchParams?.message && (
           <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
             {searchParams.message}
           </p>
         )}
+      </form>
+      <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
+      <button
+          formAction={googleSignIn}
+          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
+        >
+          Sign In with Google
+      </button>
       </form>
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -33,10 +33,10 @@ export default async function Index() {
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
-        <main className="flex-1 flex flex-col gap-6">
+        {/* <main className="flex-1 flex flex-col gap-6">
           <h2 className="font-bold text-4xl mb-4">Next steps</h2>
           {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-        </main>
+        </main> */}
       </div>
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
